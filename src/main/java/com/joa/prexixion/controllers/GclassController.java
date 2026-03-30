4222065542feef77ed78d400609c5fbdc77a4fbf
Joaquin <joacotse@gmail.com>
2026-03-29 22:36:35 -0500

    New Gclass endpoints

--- src/main/java/com/joa/prexixion/controllers/GclassController.java
+++ src/main/java/com/joa/prexixion/controllers/GclassController.java
@@ -8,6 +8,9 @@ import com.joa.prexixion.entities.Gclass;
 import com.joa.prexixion.services.GclassService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,11 @@ public class GclassController {
         return gclassService.listPlusEmpty(tableName);
     }
 
+    @GetMapping("/plusEmptyOrderById")
+    public List<Gclass> listPlusEmptyOrderById(@RequestParam String tableName) {
+        return gclassService.listPlusEmptyOrderById(tableName);
+    }
+
     @GetMapping("/listLoginOptions")
     public List<Gclass> listLoginOptions(@RequestParam String tableName) {
         return gclassService.listLoginOptions(tableName);
@@ -47,6 +55,66 @@ public class GclassController {
     @GetMapping("/listLoginOptionsSpecials")
     public List<Gclass> listLoginOptionsSpecials(@RequestParam String tableName) {
         return gclassService.listLoginOptionsSpecials(tableName);
+    }
+
+    @GetMapping("/abbr")
+    public List<Gclass> listAbbr(@RequestParam String tableName) {
+        return gclassService.listAbbr(tableName);
+    }
+
+    @GetMapping("/codigoDescripcion")
+    public List<Gclass> listCodigoDescripcion(@RequestParam String tableName) {
+        return gclassService.listCodigoDescripcion(tableName);
+    }
+
+    @PostMapping("/insertUpdate")
+    public int insertUpdate(@RequestParam String tableName, @RequestBody Gclass obj) {
+        return gclassService.insertUpdate(tableName, obj);
+    }
+
+    @PostMapping("/insertUpdate2")
+    public int insertUpdate2(@RequestParam String tableName, @RequestBody Gclass obj) {
+        return gclassService.insertUpdate2(tableName, obj);
+    }
+
+    @DeleteMapping("/delete")
+    public int delete(@RequestParam String tableName, @RequestParam int id) {
+        return gclassService.delete(tableName, id);
+    }
+
+    @GetMapping("/existe")
+    public boolean existe(@RequestParam String tableName, @RequestParam int id) {
+        return gclassService.existe(tableName, id);
+    }
+
+    @GetMapping("/getOne")
+    public Gclass getOne(@RequestParam String tableName, @RequestParam int id) {
+        return gclassService.getOne(tableName, id);
+    }
+
+    @GetMapping("/getOne2")
+    public Gclass getOne2(@RequestParam String tableName, @RequestParam int id) {
+        return gclassService.getOne2(tableName, id);
+    }
+
+    @GetMapping("/credenciales")
+    public List<Gclass> listCredenciales(@RequestParam int id) {
+        return gclassService.listCredenciales(id);
+    }
+
+    @GetMapping("/vlanIp")
+    public Gclass vlanIp(@RequestParam int ip) {
+        return gclassService.vlanIp(ip);
+    }
+
+    @GetMapping("/fromTwoTables")
+    public List<Gclass> listFromTwoTables(@RequestParam String tabla1, @RequestParam String tabla2, @RequestParam int id) {
+        return gclassService.listFromTwoTables(tabla1, tabla2, id);
+    }
+
+    @GetMapping("/fromThreeTables")
+    public List<Gclass> listFromThreeTables(@RequestParam String tabla1, @RequestParam String tabla2, @RequestParam int id1, @RequestParam int id2) {
+        return gclassService.listFromThreeTables(tabla1, tabla2, id1, id2);
     }
 
 }
--- src/main/java/com/joa/prexixion/repositories/GclassRepository.java
+++ src/main/java/com/joa/prexixion/repositories/GclassRepository.java
@@ -10,6 +10,7 @@ import com.joa.prexixion.entities.Gclass;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class GclassRepository {
@@ -100,6 +101,30 @@ public class GclassRepository {
         return gclasses;
     }
 
+    public List<Gclass> listPlusEmptyOrderById(String tableName) {
+        String sql = "SELECT id, descripcion FROM " + tableName;
+        Query query = entityManager.createNativeQuery(sql);
+
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+
+        // Crear objeto vacío
+        Gclass gclassVacio = new Gclass();
+        gclassVacio.setId(0);
+        gclassVacio.setDescripcion("-");
+
+        gclasses.add(0, gclassVacio);
+
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setDescripcion((String) result[1]);
+            gclasses.add(gclass);
+        }
+
+        return gclasses;
+    }
+
     // LOGIN PROCESOS CUSTOM ORDER
     // 1: SI
     // 0: NO
@@ -149,6 +174,207 @@ public class GclassRepository {
             gclasses.add(gclass);
         }
 
+        return gclasses;
+    }
+
+    public List<Gclass> listAbbr(String tableName) {
+        String sql = "SELECT id, abreviatura, descripcion FROM " + tableName;
+        Query query = entityManager.createNativeQuery(sql);
+
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setAbreviatura((String) result[1]);
+            gclass.setDescripcion((String) result[2]);
+            gclasses.add(gclass);
+        }
+
+        return gclasses;
+    }
+
+    public List<Gclass> listCodigoDescripcion(String tableName) {
+        String sql = "SELECT id, codigo as abreviatura, descripcion FROM " + tableName;
+        Query query = entityManager.createNativeQuery(sql);
+
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setAbreviatura((String) result[1]);
+            gclass.setDescripcion((String) result[2]);
+            gclasses.add(gclass);
+        }
+
+        return gclasses;
+    }
+
+    @Transactional
+    public int insertUpdate(String tableName, Gclass obj) {
+        int msg = 0;
+        String sqlCheck = "SELECT count(*) FROM " + tableName + " WHERE id = :id";
+        Query queryCheck = entityManager.createNativeQuery(sqlCheck);
+        queryCheck.setParameter("id", obj.getId());
+        Number count = (Number) queryCheck.getSingleResult();
+        
+        if (count.intValue() > 0) {
+            String sqlUpdate = "UPDATE " + tableName + " SET abreviatura = :abbr, descripcion = :desc WHERE id = :id";
+            Query queryUpdate = entityManager.createNativeQuery(sqlUpdate);
+            queryUpdate.setParameter("abbr", obj.getAbreviatura());
+            queryUpdate.setParameter("desc", obj.getDescripcion());
+            queryUpdate.setParameter("id", obj.getId());
+            queryUpdate.executeUpdate();
+            msg = 2; // Updated
+        } else {
+            String sqlInsert = "INSERT INTO " + tableName + " (abreviatura, descripcion) VALUES (:abbr, :desc)";
+            Query queryInsert = entityManager.createNativeQuery(sqlInsert);
+            queryInsert.setParameter("abbr", obj.getAbreviatura());
+            queryInsert.setParameter("desc", obj.getDescripcion());
+            queryInsert.executeUpdate();
+            msg = 1; // Inserted
+        }
+        return msg;
+    }
+
+    @Transactional
+    public int insertUpdate2(String tableName, Gclass obj) {
+        int rpta = 0;
+        String sqlCheck = "SELECT count(*) FROM " + tableName + " WHERE id = :id";
+        Query queryCheck = entityManager.createNativeQuery(sqlCheck);
+        queryCheck.setParameter("id", obj.getId());
+        Number count = (Number) queryCheck.getSingleResult();
+        
+        if (count.intValue() > 0) {
+            String sqlUpdate = "UPDATE " + tableName + " SET descripcion = :desc WHERE id = :id";
+            Query queryUpdate = entityManager.createNativeQuery(sqlUpdate);
+            queryUpdate.setParameter("desc", obj.getDescripcion());
+            queryUpdate.setParameter("id", obj.getId());
+            queryUpdate.executeUpdate();
+            rpta = 2; // Updated
+        } else {
+            String sqlInsert = "INSERT INTO " + tableName + " (descripcion) VALUES (:desc)";
+            Query queryInsert = entityManager.createNativeQuery(sqlInsert);
+            queryInsert.setParameter("desc", obj.getDescripcion());
+            queryInsert.executeUpdate();
+            rpta = 1; // Inserted
+        }
+        return rpta;
+    }
+
+    @Transactional
+    public int delete(String tableName, int id) {
+        String sqlDelete = "DELETE FROM " + tableName + " WHERE id = :id";
+        Query queryDelete = entityManager.createNativeQuery(sqlDelete);
+        queryDelete.setParameter("id", id);
+        queryDelete.executeUpdate();
+        return 3; // Deleted
+    }
+
+    public boolean existe(String tableName, int id) {
+        String sql = "SELECT count(*) FROM " + tableName + " WHERE id = :id";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id", id);
+        Number count = (Number) query.getSingleResult();
+        return count.intValue() > 0;
+    }
+
+    public Gclass getOne(String tableName, int id) {
+        String sql = "SELECT id, descripcion FROM " + tableName + " WHERE id = :id";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id", id);
+        
+        List<Object[]> results = query.getResultList();
+        Gclass gclass = new Gclass();
+        if(!results.isEmpty()) {
+            Object[] result = results.get(0);
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setDescripcion((String) result[1]);
+        }
+        return gclass;
+    }
+
+    public Gclass getOne2(String tableName, int id) {
+        String sql = "SELECT id, abreviatura, descripcion FROM " + tableName + " WHERE id = :id";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id", id);
+        
+        List<Object[]> results = query.getResultList();
+        Gclass gclass = new Gclass();
+        if(!results.isEmpty()) {
+            Object[] result = results.get(0);
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setAbreviatura((String) result[1]);
+            gclass.setDescripcion((String) result[2]);
+        }
+        return gclass;
+    }
+
+    public List<Gclass> listCredenciales(int id) {
+        String sql = "{call credencialesEquipo(:id)}";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id", id);
+        
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setAbreviatura((String) result[1]); // username
+            gclass.setDescripcion((String) result[2]); // clave
+            gclasses.add(gclass);
+        }
+        return gclasses;
+    }
+
+    public Gclass vlanIp(int ip) {
+        String sql = "select v.descripcion as VLAN from EquiposIp i "
+                + "inner join EquiposVlan v on i.idVlan = v.id "
+                + "where i.id= :ip";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("ip", ip);
+        
+        List<String> results = query.getResultList();
+        Gclass gclass = new Gclass();
+        if (!results.isEmpty()) {
+            gclass.setDescripcion(results.get(0));
+        }
+        return gclass;
+    }
+
+    public List<Gclass> listFromTwoTables(String tabla1, String tabla2, int id) {
+        String sql = "select id, descripcion from " + tabla1 + " where id in (select b from " + tabla2 + " where a = :id) order by descripcion";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id", id);
+        
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setDescripcion((String) result[1]);
+            gclasses.add(gclass);
+        }
+        return gclasses;
+    }
+
+    public List<Gclass> listFromThreeTables(String tabla1, String tabla2, int id1, int id2) {
+        String sql = "select id, descripcion from " + tabla1 + " where id in (select z from " + tabla2 + " where x = :id1 and y = :id2) order by descripcion";
+        Query query = entityManager.createNativeQuery(sql);
+        query.setParameter("id1", id1);
+        query.setParameter("id2", id2);
+        
+        List<Object[]> results = query.getResultList();
+        List<Gclass> gclasses = new ArrayList<>();
+        for (Object[] result : results) {
+            Gclass gclass = new Gclass();
+            gclass.setId(((Number) result[0]).intValue());
+            gclass.setDescripcion((String) result[1]);
+            gclasses.add(gclass);
+        }
         return gclasses;
     }
 
